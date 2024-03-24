32b4222d5ad9393ddd7170068f20e3b932dec3de
Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>
2024-03-24 18:22:18 +0530

    Update settings.py
    Signed-off-by: Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>

--- EMS/settings.py
+++ EMS/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'EMS.wsgi.application'
 
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
