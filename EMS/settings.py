9871f7e59e41128ff3a6c5844b0936d40e184ba8
Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>
2024-04-10 22:46:31 +0530

    Update settings.py
    Signed-off-by: Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>

--- EMS/settings.py
+++ EMS/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'EMS.urls'
+ROOT_URLCONF = 'django_app.urls'
 
 TEMPLATES = [
     {
@@ -72,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'EMS.wsgi.application'
+WSGI_APPLICATION = 'django_app.wsgi.application'
 
 
 # Database
