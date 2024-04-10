8ddf9b722348e5b1f7e1b424699e25ffb3acfe72
Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>
2024-04-10 22:45:45 +0530

    Update settings.py
    Signed-off-by: Meet Jamsutkar <91883747+MeJaM35@users.noreply.github.com>

--- EMS/settings.py
+++ EMS/settings.py
@@ -80,8 +80,7 @@ WSGI_APPLICATION = 'EMS.wsgi.application'
 
 
 
-
- DATABASES = {
+DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': BASE_DIR / 'db.sqlite3',
